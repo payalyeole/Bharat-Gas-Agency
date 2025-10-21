9ef59f5ffbafb45b84d43185a73222c365b89ccf
Payal Yeole <yeolepayal366@gmail.com>
2025-10-21 08:38:20 +0530

    Added Agency Details

--- src/gasSupplier/gasAgency.java
+++ src/gasSupplier/gasAgency.java
@@ -1,4 +1,13 @@
 package gasSupplier;
 
 public interface gasAgency {
+    public String agencyName = "Bharat Gas";
+    public int agencyCode = 1234;
+    public int phNumber = 875555;
+
+    default void agencyDesplay(){
+        System.out.println("The agency name is "+agencyName);
+        System.out.println("The agency Code is "+agencyCode);
+        System.out.println("The agency phone Number is "+phNumber);
+    }
 }
