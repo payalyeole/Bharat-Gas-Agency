e6c849ebc1ebb37805f6cf1068ad779e31974184
Payal Yeole <yeolepayal366@gmail.com>
2025-10-21 08:57:01 +0530

    Added Customer Details

---
+++ src/Customers/Customer.java
@@ -0,0 +1,20 @@
+package Customers;
+
+import gasSupplier.gasAgency;
+
+public class Customer {
+    public String name;
+    public String Street;
+    public String area;
+    public int pincode;
+    public int mobile;
+
+    public Customer(String name, String street, String area, int pincode, int mobile) {
+        this.name = name;
+        Street = street;
+        this.area = area;
+        this.pincode = pincode;
+        this.mobile = mobile;
+    }
+
+}
