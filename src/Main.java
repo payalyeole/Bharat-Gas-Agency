66be0c201537de6ddc1393be10f40f410473c6df
Payal Yeole <yeolepayal366@gmail.com>
2025-10-23 11:54:55 +0530

    Added code for check late delivery

--- src/Main.java
+++ src/Main.java
@@ -26,6 +26,24 @@ public class Main {
         System.out.println("\n");
     }
 
+    public static void checkLateDel(Delivery[] obj){
+        String[] months = new String[]{"January" , "February" , "March" , "April" ,
+                "May" , "June" , "July" , "August" , "September" , "October" , "November" , "December"};
+        int[] month = new int[12];
+        for (Delivery delivery: obj){
+            if (delivery.Status.equals("0") && delivery.amount == 783.75 ){
+                month[delivery.dt_2.getMonth()] += 1;
+            }
+        }
+        System.out.println("------------------late delivery-----------------");
+        for (int i=0;i<12;i++){
+            if (month[i] != 0){
+                System.out.println(" * In "+ months[i]+"there are "+month[i]);
+            }
+        }
+        System.out.println("\n");
+    }
+
     public static void main(String[] args) {
         System.out.println("Hey Dear!");
     }
